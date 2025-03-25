f765718049ca9449de8b406208399103b871a10a
vicky-ogutu <vikogutu77@gmail.com>
2025-03-26 00:54:01 +0300

    try/catch

--- lib/Trials/http_post_screen.dart
+++ lib/Trials/http_post_screen.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart'as http;
 
@@ -11,6 +13,28 @@ class PostData extends StatefulWidget {
 
 class _PostDataState extends State<PostData> {
   TextEditingController title =TextEditingController();
+  TextEditingController body =TextEditingController();
+
+  Future<void> postUser()async {
+    String baseUrl ="https://jsonplaceholder.typicode.com/posts";
+    Map<String, dynamic> post_data={
+      "title": title.text,
+      "body": body.text
+    };
+    try{
+       final response =await http.post(Uri.parse(baseUrl),
+       headers: {"Content-Type":"application/json"},
+       body: jsonEncode(post_data)  
+       );
+
+
+
+    }catch(Error){
+      print(Error.toString());
+
+    }
+   // return;
+  }
 
   @override
   Widget build(BuildContext context) {
