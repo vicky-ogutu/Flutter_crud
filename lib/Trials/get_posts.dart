82f3c30b95596f03d29846995d3cc6bf8d3be7f3
vicky-ogutu <vikogutu77@gmail.com>
2025-03-27 00:18:28 +0300

    successful_get_msg

--- lib/Trials/get_posts.dart
+++ lib/Trials/get_posts.dart
@@ -1,6 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_app/post.dart';
+import 'package:http/http.dart' as http;
 
 class Getposts extends StatefulWidget {
   const Getposts({super.key});
@@ -16,9 +19,13 @@ class _GetpostsState extends State<Getposts> {
     return const Placeholder();
   }
 
-  Future<List<Post>> getPosts(){
+  Future<List<Post>> getPosts() async{
     final String baseURL ="https://jsonplaceholder.typicode.com/posts";
     try{
+      final response = await http.get(Uri.parse(baseURL));
+      if(response.statusCode==200){
+
+      }
 
     }catch(Error){
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Netork error: $Error")));
